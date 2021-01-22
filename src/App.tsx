c974e1c0d9deffcf0f3a9cbbf2e0bdb07638fca3
Wayne Marsh <bits@waynemarsh.com>
2021-01-22 00:46:32 -0500

    Button example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
-import { AppLayout, Badge, Button } from '@awsui/components-react';
+import { Button } from '@awsui/components-react';
 
 function App() {
   return (
     <div className="App">
       <main>
-        <AppLayout content={
-          <div>
-
-            <Button onClick={() => window.alert("I was clicked")}>I am a button!</Button>
-            <Badge color="blue">I am a badge!</Badge>
-          </div>
-
-        }/>
+        <Button>I am a button!</Button>
       </main>
     </div>
   );
